7fd088998f32062b64cfa3e3dcab10a281250232
R. Kukuh <rkukuh@gmail.com>
2018-11-08 11:33:09 +0700

    Create external js

---
+++ pertemuan07/js/app.js
@@ -0,0 +1,10 @@
+alert('ini dari external JS');
+
+prompt('Siapa nama Anda?');
+
+console.log('ERROR: Disconnected');
+console.log(5678);
+console.log(-67);
+console.log(0.6789);
+console.log(true);
+console.log(false);
